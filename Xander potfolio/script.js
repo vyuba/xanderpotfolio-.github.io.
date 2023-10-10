ff8a1ba1e4a3c0b76b96db21f684223145c09456
vyuba <118875235+vyuba@users.noreply.github.com>
2023-10-10 20:17:41 +0100

    Add files via upload

--- Xander potfolio/script.js
+++ Xander potfolio/script.js
@@ -1,8 +1,15 @@
 const ham = document.getElementsByClassName('hamburger')[0];
 const nav = document.getElementsByClassName('navbarlinks')[0];
 const navBar = document.getElementsByClassName("navbar")[0];
+const sidebar = document.getElementsByClassName("sidebar")[0];
+const close = document.getElementsByClassName("hamburger-close")[0];
 
 ham.addEventListener('click', () => {
+    sidebar.style.display= 'flex';
     nav.classList.toggle('active');
     navBar.classList.toggle('active');
+});
+
+close.addEventListener("click", () => {
+  sidebar.style.display = "none";
 });
